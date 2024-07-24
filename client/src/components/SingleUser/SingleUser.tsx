7ae34f5d176fed86b71febdcf065ebe64ab7c46b
ajummer <makeudreamempty@gmail.com>
2024-07-24 13:18:48 +0300

    feat: create single user component

---
+++ client/src/components/SingleUser/SingleUser.tsx
@@ -0,0 +1,20 @@
+export default function SingleUser() {
+  return (
+    <tr>
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap">3</p>
+      </td>
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <div className="flex items-center">
+          <p className="whitespace-no-wrap">Besique Monroe</p>
+        </div>
+      </td>
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap">Administrator</p>
+      </td>
+      <td className="border-b border-gray-200 bg-white px-5 py-5 text-sm">
+        <p className="whitespace-no-wrap">Sep 28, 2022</p>
+      </td>
+    </tr>
+  );
+}
